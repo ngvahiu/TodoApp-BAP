dff9bbf19fed49d4384735813e7ba616bc8e98e4
ngvahiu <vanhieu230303@gmail.com>
2024-05-21 11:51:00 +0700

    Add update todo

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,74 @@
 "use strict";
 var inputBox = document.getElementById("input-box");
+var inputUpdate = document.getElementById("update-input");
 var button = document.querySelector("button");
 var list = document.getElementById("list-container");
+var currentTodo = null;
+// Add Task
 function addTask() {
     if (inputBox.value === '') {
         alert("you must write something!");
     }
     else {
         var li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        var p = document.createElement("p");
+        p.innerText = inputBox.value;
+        li.appendChild(p);
         list.appendChild(li);
         inputBox.value = '';
+        var button_1 = document.createElement("button");
+        button_1.innerHTML = "Update";
         var span = document.createElement("span");
         span.innerHTML = "x";
-        li.appendChild(span);
+        var div = document.createElement("div");
+        div.appendChild(button_1);
+        div.appendChild(span);
+        li.appendChild(div);
     }
     saveData();
 }
+button.addEventListener("click", addTask);
 list.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
+    var _a, _b, _c, _d, _e, _f;
+    var target = e.target;
+    if (target.tagName === "LI") {
+        target.classList.toggle("checked"); // Update status of task
         saveData();
     }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
+    else if (target.tagName === "SPAN") { // Delete task
+        (_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement.remove();
         saveData();
+    }
+    else if (target.tagName === "BUTTON") { // Turn on Modal to update task
+        var modal_1 = document.querySelector(".modal");
+        var span = modal_1 === null || modal_1 === void 0 ? void 0 : modal_1.querySelector(".close");
+        var input = document.getElementById("update-input");
+        input.value = (_e = (_d = (_c = (_b = target.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.querySelector('p')) === null || _d === void 0 ? void 0 : _d.innerText) !== null && _e !== void 0 ? _e : '';
+        currentTodo = (_f = target.parentElement) === null || _f === void 0 ? void 0 : _f.parentElement;
+        modal_1.style.display = "block";
+        span.onclick = function () {
+            modal_1.style.display = "none";
+        };
+        window.onclick = function (event) {
+            if (event.target == modal_1) {
+                modal_1.style.display = "none";
+            }
+        };
     }
 });
-button.addEventListener("click", addTask);
+// Update Task
+inputUpdate.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        if (currentTodo) {
+            currentTodo.querySelector('p').innerText = inputUpdate.value;
+            saveData();
+            inputUpdate.value = '';
+            var modal = document.querySelector(".modal");
+            modal.style.display = "none";
+        }
+    }
+});
 function saveData() {
     localStorage.setItem("data", list.innerHTML);
 }
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,79 @@
 const inputBox: HTMLInputElement = document.getElementById("input-box") as HTMLInputElement;
+const inputUpdate: HTMLInputElement = document.getElementById("update-input") as HTMLInputElement;
 const button: HTMLButtonElement = document.querySelector("button")!;
 const list: HTMLUListElement = document.getElementById("list-container") as HTMLUListElement;
 
+let currentTodo: HTMLLIElement | null = null;
 
+// Add Task
 function addTask(): void {
     if (inputBox.value === '') {
         alert("you must write something!");
     } else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
+        let li: HTMLLIElement = document.createElement("li");
+        let p: HTMLParagraphElement = document.createElement("p");
+        p.innerText = inputBox.value;
+        li.appendChild(p);
         list.appendChild(li);
         inputBox.value = '';
-        let span = document.createElement("span");
+
+        let button: HTMLButtonElement = document.createElement("button");
+        button.innerHTML = "Update";
+        let span: HTMLSpanElement = document.createElement("span");
         span.innerHTML = "x";
-        li.appendChild(span);
+        let div: HTMLDivElement = document.createElement("div");
+        div.appendChild(button);
+        div.appendChild(span);
+
+        li.appendChild(div);
     }
     saveData();
 }
 button.addEventListener("click", addTask);
 
 list.addEventListener("click", (e: MouseEvent) => {
-    if ((e.target as HTMLElement).tagName === "LI") {
-        (e.target as HTMLElement).classList.toggle("checked");
+    const target = e.target as HTMLElement;
+    if (target.tagName === "LI") {
+        target.classList.toggle("checked"); // Update status of task
         saveData();
-    } else if ((e.target as HTMLElement).tagName === "SPAN") {
-        (e.target as HTMLElement).parentElement!.remove();
+    } else if (target.tagName === "SPAN") { // Delete task
+        target.parentElement?.parentElement!.remove();
         saveData();
+    } else if (target.tagName === "BUTTON") { // Turn on Modal to update task
+        let modal: HTMLDivElement = document.querySelector(".modal") as HTMLDivElement;
+        let span = modal?.querySelector(".close") as HTMLSpanElement;
+        let input = document.getElementById("update-input") as HTMLInputElement;
+        input.value = target.parentElement?.parentElement?.querySelector('p')?.innerText ?? '';
+        currentTodo = target.parentElement?.parentElement as HTMLLIElement | null;
+
+        modal.style.display = "block";
+
+        span.onclick = () => {
+            modal.style.display = "none";
+        }
+
+        window.onclick = (event) => {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
     }
 });
+
+// Update Task
+inputUpdate.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        if (currentTodo) {
+            currentTodo.querySelector('p')!.innerText = inputUpdate.value;
+            saveData();
+            inputUpdate.value = '';
+            const modal: HTMLDivElement = document.querySelector(".modal") as HTMLDivElement;
+            modal.style.display = "none";
+        }
+    }
+});
+
 
 function saveData(): void {
     localStorage.setItem("data", list.innerHTML);
